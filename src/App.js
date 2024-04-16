901ff2e02c142e27f9c9755abff9fbb7dcd937c2
Gennady Sidorenko <sidorenqo@gmail.com>
2024-04-17 01:12:52 +0500

    добавленые пропсы, часть css стилей переведены на модули

--- src/App.js
+++ src/App.js
@@ -1,8 +1,71 @@
 import Card from "./components/Card";
-import Header from "./components/Header";
-import Drawer from "./components/Drawer";
+import Header from "./components/Header/Header";
+import Drawer from "./components/Drawer/Drawer";
 
-function App() {
+const productCards = [
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    imageUrl: "/img/sneakers/1.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+    imageUrl: "/img/sneakers/2.jpg",
+  },
+  {
+    title: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    imageUrl: "/img/sneakers/3.jpg",
+  },
+  {
+    title: "Кроссовки Puma X Aka Boku Future Rider",
+    price: 8999,
+    imageUrl: "/img/sneakers/4.jpg",
+  },
+  // {
+  //   title: "Мужские Кроссовки Under Armour Curry 8",
+  //   price: 15199,
+  //   imageUrl: "/img/sneakers/5.jpg",
+  // },
+  // {
+  //   title: "Мужские Кроссовки Nike Kyrie 7",
+  //   price: 11299,
+  //   imageUrl: "/img/sneakers/6.jpg",
+  // },
+  // {
+  //   title: "Мужские Кроссовки Jordan Air Jordan 11",
+  //   price: 10799,
+  //   imageUrl: "/img/sneakers/7.jpg",
+  // },
+  // {
+  //   title: "Мужские Кроссовки Nike LeBron XVIII",
+  //   price: 16499,
+  //   imageUrl: "/img/sneakers/8.jpg",
+  // },
+  // {
+  //   title: "Мужские Кроссовки Nike Lebron XVIII Low",
+  //   price: 13999,
+  //   imageUrl: "/img/sneakers/9.jpg",
+  // },
+  // {
+  //   title: "Мужские Кроссовки Nike Blazer Mid Suede",
+  //   price: 8499,
+  //   imageUrl: "/img/sneakers/10.png",
+  // },
+  // {
+  //   title: "Кроссовки Puma X Aka Boku Future Rider",
+  //   price: 8999,
+  //   imageUrl: "/img/sneakers/11.png",
+  // },
+  // {
+  //   title: "Мужские Кроссовки Nike Kyrie Flytrap IV",
+  //   price: 11299,
+  //   imageUrl: "/img/sneakers/12.jpg",
+  // },
+];
+
+function App(props) {
   return (
     <div className="wrapper clear">
       <Drawer />
@@ -17,7 +80,13 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card />
+          {productCards.map((card) => (
+            <Card
+              title={card.title}
+              price={card.price}
+              imageUrl={card.imageUrl}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="d-flex justify-between align-center p-40">
       <div className="d-flex align-center">
-        <img width={40} height={40} src="/img/logo.png" />
+        <img width={40} height={40} src="/img/logo.png" alt="logo" />
         <div>
           <h3 className="clear text-uppercase">React Sneakers</h3>
           <p className="opacity-5">Магазин лучших кросовок</p>
@@ -13,14 +13,21 @@ function Header() {
       <div>
         <ul className="d-flex ">
           <li className="mr-30">
-            <img width={18} height={18} src="/img/svg/cart.svg" />
+            <img width={18} height={18} src="/img/svg/cart.svg" alt="cart" />
             <span>0000 рублей</span>
           </li>
           <li className="mr-30">
-            <img width={18} height={18} src="/img/svg/favorit.svg" />
+            <img
+              width={18}
+              height={18}
+              src="/img/svg/favorite.svg"
+              alt="favorite"
+            />
+            <span>Закладки</span>
           </li>
           <li>
-            <img width={18} height={18} src="/img/svg/user.svg" alt="" />
+            <img width={18} height={18} src="/img/svg/user.svg" alt="user" />
+            <span>Профиль</span>
           </li>
         </ul>
       </div>
